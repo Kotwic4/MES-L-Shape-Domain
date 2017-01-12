433b0a31a77a2bd633a60e207407dac9d062d865
Kotwic4 <Kotwic4@gmail.com>
2017-01-12 12:50:22 +0100

    add testing mode + small fix

--- MES.py
+++ MES.py
@@ -6,13 +6,34 @@ import scipy.interpolate as interp
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
+test = False
+e = 0.00001
+
 
 def g(x, y):
-    r = math.sqrt(x ** 2 + y ** 2)
-    cos = x/r  # sin(fi + pi/2) = cos(fi)
-    if y <= 0 or x <= 0:
-        cos = -cos
-    return (r ** 2) ** (1/3) * (cos ** 2) ** (1/3)
+    if test:
+        if (abs(y + 1.0) < e) and (x >= 0):
+            return -x
+        if (abs(x + 1.0) < e) and (y >= 0):
+            return -y
+        if abs(y - 1.0) < e:
+            return x
+        if abs(x - 1.0) < e:
+            return y
+    else:
+        r = math.sqrt(x ** 2 + y ** 2)
+        cos = x/r  # sin(fi + pi/2) = cos(fi)
+        if y <= 0 or x <= 0:
+            cos = -cos
+        return (r ** 2) ** (1/3) * (cos ** 2) ** (1/3)
+
+
+def getx(i):
+    return (i % n_a)*h_a-1
+
+
+def gety(i):
+    return 1-(i // n_a)*h_b
 
 
 def read_parameter(label):
@@ -32,9 +53,9 @@ b, n_b, h_b, d_b = read_parameter("b")
 nab = n_a * n_b
 
 MatrixA = [[0 for x in range(nab)] for y in range(nab)]
-a1 = 2/3 * a * b
-a2 = -1/6 * a * b
-a3 = -1/3 * a * b
+a1 = 2/3 * (a * b)
+a2 = -1/6 * (a * b)
+a3 = -1/3 * (a * b)
 Matrix = [[a1, a2, a3, a2],
           [a2, a1, a2, a3],
           [a3, a2, a1, a2],
@@ -62,33 +83,38 @@ for i in range(b, n_b):
         MatrixA[x][x] = 1
 
 for i in range(1, n_a-1):
-    x = -1 + (i*h_a)
-    y = 1
     index = i
-    MatrixB[index] = (1/2) * h_a * (g(x-d_a, 1) + g(x+d_a, 1))
+    x = getx(index)
+    y = gety(index)
+    MatrixB[index] = (1/2) * h_b * (g(x-d_a, y) + g(x+d_a, y))
 for i in range(1, n_b-1):
-    x = 1
-    y = 1 - (i*h_b)
-    index = (i+1)*n_a-1
-    MatrixB[index] = (1/2) * h_b * (g(x, y+d_b) + g(x, y-d_b))
+    index = (i + 1) * n_a - 1
+    x = getx(index)
+    y = gety(index)
+    MatrixB[index] = (1/2) * h_a * (g(x, y+d_b) + g(x, y-d_b))
 for i in range(1, b):
-    x = -1
-    y = 1 - (i * h_b)
     index = i*n_a
-    MatrixB[index] = (1/2) * h_b * (g(x, y+d_b) + g(x, y-d_b))
+    x = getx(index)
+    y = gety(index)
+    MatrixB[index] = (1/2) * h_a * (g(x, y+d_b) + g(x, y-d_b))
 for i in range(a+1, n_a-1):
-    x = -1 + (i*h_a)
-    y = -1
     index = nab-n_a+i
-    MatrixB[index] = (1/2) * h_a * (g(x-d_a, -1) + g(x+d_a, -1))
-MatrixB[0] = (1/2) * (g(-1+d_a, 1) * h_a + g(-1, 1-d_b) * h_b)
-MatrixB[n_a-1] = (1/2) * (g(1-d_a, 1) * h_a + g(1, 1-d_b) * h_b)
-MatrixB[nab-1] = (1/2) * (g(1-d_a, -1) * h_a + g(1, -1+d_b) * h_b)
-for i in range(0, nab):
-        MatrixB[i] = (MatrixB[i])*a*b
+    x = getx(index)
+    y = gety(index)
+    MatrixB[index] = (1/2) * h_b * (g(x-d_a, -1) + g(x+d_a, -1))
+MatrixB[0] = (1/2) * (g(-1+d_a, 1) * h_b + g(-1, 1-d_b) * h_a)
+MatrixB[n_a-1] = (1/2) * (g(1-d_a, 1) * h_b + g(1, 1-d_b) * h_a)
+MatrixB[nab-1] = (1/2) * (g(1-d_a, -1) * h_b + g(1, -1+d_b) * h_a)
 MatrixZ = np.linalg.solve(MatrixA, MatrixB)
-X = [(x % n_a)*h_a-1 for x in range(nab)]
-Y = [1-(y // n_a)*h_b for y in range(nab)]
+for i in range(0,nab):
+    MatrixZ[i] *= a * b
+    if test:
+        x = getx(i)
+        y = gety(i)
+        if x >= 0 or y >= 0:
+            MatrixZ[i] -= x * y
+X = [getx(i) for i in range(nab)]
+Y = [gety(i) for i in range(nab)]
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
@@ -110,6 +136,3 @@ surf = ax.plot_surface(
     linewidth=0)
 
 plt.show()
-
-
-
